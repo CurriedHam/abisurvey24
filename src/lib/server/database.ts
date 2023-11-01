1bdbb558aadb7d1da39b1948eb7497ed87e1eba9
CurriedHam <lukas.ufer@gmx.de>
2023-11-01 20:30:17 +0100

    emv-variabeln

--- src/lib/server/database.ts
+++ src/lib/server/database.ts
@@ -4,7 +4,7 @@ const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = env;
 import { Sequelize } from "sequelize";
 
 function connectDB() {
-	return new Sequelize(`postgres://${"abisurvey"}:${"abisurvey"}@${"host.docker.internal"}:${5432}/${"abisurvey"}`);
+	return new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`);
 }
 
 export const db = connectDB();
